2a5fc4b3191ec371355c0cf39f7c1f16d59c12a7
Seggy116 <59027059+Seggys116@users.noreply.github.com>
2022-03-22 16:56:32 +0000

    Revert "Initial Commit"
    This reverts commit 43e5102771f85319b7f3e1bbf936a376a33aa468.

--- assets/js/Footer.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('#heightControl').css('height', $(window).height() - $('html').height() +'px');
-});
-
-$(window).resize(function() {
-    $('#heightControl').css('height', $(window).height() - $('html').height() +'px');
-});
